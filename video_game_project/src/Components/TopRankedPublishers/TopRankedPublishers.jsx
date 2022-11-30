99953ee73b3c1aaeaa8e7446c531f7606222c838
TSBrinker <tsbrinker@gmail.com>
2022-11-30 15:15:58 -0600

    cleared out long former code for legibility

--- video_game_project/src/Components/TopRankedPublishers/TopRankedPublishers.jsx
+++ video_game_project/src/Components/TopRankedPublishers/TopRankedPublishers.jsx
@@ -1,22 +1,4 @@
 const TopRankedPublishers = ({ allGames }) => {
-  // get list of publishers
-  //get number of games by each publisher
-  // total game rank
-  //average it out
-  // voila
-
-  let nintendoGames = 0;
-  let activisionGames = 0;
-  let takeTwoInteractiveGames = 0;
-  let sonyComputerEntertainmentGames = 0;
-  let microsoftGameStudiosGames = 0;
-  let ubisoftGames = 0;
-  let electronicArtsGames = 0;
-  let atariGames = 0;
-  let squareSoftGames = 0;
-  let bethesdaSoftworksGames = 0;
-  let segaGames = 0;
-
   function testTwoLists() {
     let publishers = [];
     let publisherOccurences = [];
@@ -46,46 +28,6 @@ const TopRankedPublishers = ({ allGames }) => {
 
   testTwoLists();
 
-  function listPublishers() {
-    for (let i = 0; i < allGames.length; i++) {
-      if (allGames[i].game_rank < 101) {
-        if (allGames[i].publisher === "Nintendo") {
-          nintendoGames += 1;
-        }
-        if (allGames[i].publisher === "Activision") {
-          activisionGames += 1;
-        }
-        if (allGames[i].publisher === "Take-Two Interactive") {
-          takeTwoInteractiveGames += 1;
-        }
-        if (allGames[i].publisher === "Sony Computer Entertainment") {
-          sonyComputerEntertainmentGames += 1;
-        }
-        if (allGames[i].publisher === "Microsoft Game Studios") {
-          microsoftGameStudiosGames += 1;
-        }
-        if (allGames[i].publisher === "Ubisoft") {
-          ubisoftGames += 1;
-        }
-        if (allGames[i].publisher === "Electronic Arts") {
-          electronicArtsGames += 1;
-        }
-        if (allGames[i].publisher === "Atari") {
-          atariGames += 1;
-        }
-        if (allGames[i].publisher === "SquareSoft") {
-          squareSoftGames += 1;
-        }
-        if (allGames[i].publisher === "Bethesda Softworks") {
-          bethesdaSoftworksGames += 1;
-        }
-        if (allGames[i].publisher === "Sega") {
-          segaGames += 1;
-        }
-      }
-    }
-  }
-  // listPublishers();
   return <div>We Be Chartin</div>;
 };
 
