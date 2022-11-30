fc8747f1bd7635dd03ea0aa3dabf36ece0489617
TSBrinker <tsbrinker@gmail.com>
2022-11-30 15:11:47 -0600

    reworked previous functionality with multiple loops

--- video_game_project/src/Components/TopRankedPublishers/TopRankedPublishers.jsx
+++ video_game_project/src/Components/TopRankedPublishers/TopRankedPublishers.jsx
@@ -17,6 +17,35 @@ const TopRankedPublishers = ({ allGames }) => {
   let bethesdaSoftworksGames = 0;
   let segaGames = 0;
 
+  function testTwoLists() {
+    let publishers = [];
+    let publisherOccurences = [];
+    let thisPublisher;
+    for (let i = 0; i < allGames.length; i++) {
+      if (allGames[i].game_rank < 101) {
+        if (!publishers.includes(allGames[i].publisher)) {
+          publishers.push(allGames[i].publisher);
+        }
+      }
+    }
+    for (let i = 0; i < publishers.length; i++) {
+      publisherOccurences.push(0);
+      thisPublisher = publishers[i];
+      for (let i2 = 0; i2 < allGames.length; i2++) {
+        if (allGames[i2].game_rank < 101) {
+          if (allGames[i2].publisher === thisPublisher) {
+            publisherOccurences[i] += 1;
+          }
+        }
+      }
+    }
+    for (let i = 0; i < publishers.length; i++) {
+      console.log(`${publishers[i]}: ${publisherOccurences[i]} `);
+    }
+  }
+
+  testTwoLists();
+
   function listPublishers() {
     for (let i = 0; i < allGames.length; i++) {
       if (allGames[i].game_rank < 101) {
@@ -55,21 +84,8 @@ const TopRankedPublishers = ({ allGames }) => {
         }
       }
     }
-
-    // console.log(publishers);
-    console.log(nintendoGames);
-    console.log(activisionGames);
-    console.log(takeTwoInteractiveGames);
-    console.log(sonyComputerEntertainmentGames);
-    console.log(microsoftGameStudiosGames);
-    console.log(ubisoftGames);
-    console.log(electronicArtsGames);
-    console.log(atariGames);
-    console.log(squareSoftGames);
-    console.log(bethesdaSoftworksGames);
-    console.log(segaGames);
   }
-  listPublishers();
+  // listPublishers();
   return <div>We Be Chartin</div>;
 };
 
