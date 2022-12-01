56279b800d8cd31e6cebb0915c2573ed57a8d51d
TSBrinker <tsbrinker@gmail.com>
2022-12-01 17:50:09 -0600

    Attempting to condense games so that the same game released on multiple consoles displays as a single object, but current attempt is creating an infinite loop. Commented out and committing to quit for the night

--- video_game_project/src/Components/SearchResults/SearchResults.jsx
+++ video_game_project/src/Components/SearchResults/SearchResults.jsx
@@ -1,10 +1,11 @@
-import { all } from "axios";
 import React, { useEffect, useState } from "react";
 import { Table, Button, Container } from "react-bootstrap";
 import SelectedGame from "../SelectedGame/SelectedGame";
 
 const SearchResults = ({ allGames, query }) => {
   const [filteredGames, setFilteredGames] = useState([]);
+
+  let groupedGames = [];
 
   function filterGames() {
     let games;
@@ -23,6 +24,72 @@ const SearchResults = ({ allGames, query }) => {
     });
     setFilteredGames(games);
   }
+  // console.log(filteredGames);
+
+  // for (let i = 0; i < filteredGames.length; i++) {
+  //   if (groupedGames.length === 0) {
+  //     setGroupedGames([filteredGames[i]]);
+  //   }
+  //   for (let i2 = 0; (i2 = groupedGames.length); i2++) {
+  //     if (
+  //       !(
+  //         groupedGames[i].name === filteredGames[i2] ||
+  //         groupedGames[i].name === filteredGames[i2 - 1]
+  //       )
+  //     ) {
+  //       setGroupedGames([...groupedGames, filteredGames[i]]);
+  //     }
+  //   }
+  // }
+
+  filteredGames.map((parentGame) => {
+    if (groupedGames.length > 0) {
+      groupedGames.map((game) => {
+        if (game.name != parentGame.name || groupedGames.length == 0) {
+          console.log("I'm trying to add something new!");
+          let newGameObject = {
+            game_rank: parentGame.game_rank,
+            name: parentGame.name,
+            platforms: [parentGame.platform],
+            year: parentGame.year,
+            genre: parentGame.genre,
+            publisher: parentGame.publisher,
+            northamericasales: [parentGame.northamericasales],
+            europesales: [parentGame.europesales],
+            japansales: [parentGame.japansales],
+            othersales: [parentGame.othersales],
+            globalsales: [parentGame.globalsales],
+          };
+          groupedGames.push(newGameObject);
+        } else {
+          console.log("I'm trying to update something!");
+          game.platforms.push(parentGame.platform);
+          game.northamericasales.push(parentGame.northamericasales);
+          game.europesales.push(parentGame.europesales);
+          game.japansales.push(parentGame.japansales);
+          game.othersales.push(parentGame.othersales);
+          game.globalsales.push(parentGame.globalsales);
+        }
+      });
+    } else {
+      groupedGames.push({
+        game_rank: parentGame.game_rank,
+        name: parentGame.name,
+        platforms: [parentGame.platform],
+        year: parentGame.year,
+        genre: parentGame.genre,
+        publisher: parentGame.publisher,
+        northamericasales: [parentGame.northamericasales],
+        europesales: [parentGame.europesales],
+        japansales: [parentGame.japansales],
+        othersales: [parentGame.othersales],
+        globalsales: [parentGame.globalsales],
+      });
+      console.log(groupedGames);
+    }
+  });
+
+  console.log("groupedGames", groupedGames);
 
   useEffect(() => {
     filterGames();
@@ -34,7 +101,7 @@ const SearchResults = ({ allGames, query }) => {
         <thead>
           <tr>
             <th>Title</th>
-            <th>Platform</th>
+            <th>Platform(s)</th>
             <th>Release Year</th>
             <th>Publisher</th>
             <th>Genre</th>
--- video_game_project/src/Components/TopRankedPublishers/TopRankedPublishers.jsx
+++ video_game_project/src/Components/TopRankedPublishers/TopRankedPublishers.jsx
@@ -7,13 +7,10 @@ const TopRankedPublishers = ({ allGames }) => {
     }
   });
   let top100Publishers = filteredGames.map((game) => game.publisher);
-  console.log("top100Publishers", top100Publishers);
   let distinctPublishers = [...new Set(top100Publishers)];
-  console.log("distinctPublishers", distinctPublishers);
   let publisherArrays = distinctPublishers.map((publisher) => {
     return [publisher, 0, "royalblue"];
   });
-  console.log("publisherArrays", publisherArrays);
   for (let i = 0; i < distinctPublishers.length; i++) {
     filteredGames.map((game) => {
       if (game.publisher === distinctPublishers[i]) {
@@ -21,30 +18,6 @@ const TopRankedPublishers = ({ allGames }) => {
       }
     });
   }
-
-  // function testTwoLists() {
-  //   let publishers = [];
-  //   let publisherOccurences = [];
-  //   let thisPublisher;
-  //   for (let i = 0; i < allGames.length; i++) {
-  //     if (allGames[i].game_rank < 101) {
-  //       if (!publishers.includes(allGames[i].publisher)) {
-  //         publishers.push(allGames[i].publisher);
-  //       }
-  //     }
-  //   }
-  //   for (let i = 0; i < publishers.length; i++) {
-  //     publisherOccurences.push(0);
-  //     thisPublisher = publishers[i];
-  //     for (let i2 = 0; i2 < allGames.length; i2++) {
-  //       if (allGames[i2].game_rank < 101) {
-  //         if (allGames[i2].publisher === thisPublisher) {
-  //           publisherOccurences[i] += 1;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
 
   const data = [
     ["Publishers", "Games in Top 100", { role: "style" }],
@@ -58,8 +31,6 @@ const TopRankedPublishers = ({ allGames }) => {
     },
     // legend: { position: "bottom" },
   };
-
-  // testTwoLists();
 
   return (
     <div className="d-flex justify-content-center">
