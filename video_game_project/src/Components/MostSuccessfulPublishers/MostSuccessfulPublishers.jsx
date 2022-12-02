cdab292fabd1b3c107aa827f17730bae00c6e223
Fred Morris <FredrickCPMorris@hotmail.com>
2022-12-02 15:31:16 -0600

    partner passover.

--- video_game_project/src/Components/MostSuccessfulPublishers/MostSuccessfulPublishers.jsx
+++ video_game_project/src/Components/MostSuccessfulPublishers/MostSuccessfulPublishers.jsx
@@ -15,8 +15,28 @@ const MostSuccessfulPublishers = ({ allGames }) => {
   });
   let publishers = gamesByPlatform.map((game) => game.publisher);
   let distinctPublishers = [...new Set(publishers)];
+  let publisherArrays = distinctPublishers.map((publisher) => {
+    return [publisher, 0, "royalblue"];
+  });
+  for (let i = 0; i < distinctPublishers.length; i++) {
+    gamesByPlatform.map((game) => {
+      if (game.publisher === distinctPublishers[i]) {
+        publisherArrays[i][1] += game.globalsales;
+        publisherArrays[i].sort();
+      }
+    });
+  }
+  const data = [
+    ["Publishers", "Games in Top 100", { role: "style" }],
+    ...publisherArrays,
+  ];
 
-  console.log("distinctPublishers", distinctPublishers);
+  // console.log(data);
+  // console.log("distinctPublishers", distinctPublishers);
+
+  // console.log("Games by Platform", gamesByPlatform);
+
+  console.log("Publisher Array", publisherArrays);
 
   return (
     <div>
