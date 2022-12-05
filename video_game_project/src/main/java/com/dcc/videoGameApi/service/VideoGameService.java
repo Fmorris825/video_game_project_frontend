aef5f0f2888d982093ddedc200fc47beecad8ef1
TSBrinker <tsbrinker@gmail.com>
2022-12-05 15:10:48 -0600

    added backend files so it's all in one folder

---
+++ video_game_project/src/main/java/com/dcc/videoGameApi/service/VideoGameService.java
@@ -0,0 +1,34 @@
+package com.dcc.videoGameApi.service;
+
+import com.dcc.videoGameApi.models.VideoGame;
+import com.dcc.videoGameApi.repository.VideoGameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Transactional
+@Service
+public class VideoGameService {
+
+    @Autowired
+    private VideoGameRepository videoGameRepository;
+
+    public long GetCountOfGames(){
+        return videoGameRepository.count();
+    }
+
+    public List<VideoGame> GetAllVideoGames() {
+
+        return videoGameRepository.findAll().stream().toList();
+    }
+
+    public VideoGame GetSingleGame(@PathVariable int id) {
+
+        return  videoGameRepository.findAll().stream().filter(g->g.getId()==id).findFirst().orElse(null);
+    }
+
+
+}
